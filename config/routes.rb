8dc491e132e7473ddc3ff1d42585c0ee81da3831
anthonymartin36 <anthony.martin36@gmail.com>
2025-05-28 10:39:16 +1200

    There is an error with the /api/v1 -accessing body values

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
-  get '/current_user', to: 'current_user#index'
-  devise_for :users, path: '', path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    registration: 'signup'
-  },
-  controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
-  resources :carts
-  resources :categories
+
   namespace :api do
     namespace :v1 do
+      devise_for :users, path: '', path_names: {
+        sign_in: 'login',
+        sign_out: 'logout',
+        registration: 'signup'
+      },
+      controllers: {
+        sessions: 'api/v1/users/sessions',
+        registrations: 'api/v1/users/registrations'
+      }
+      get '/current_user', to: 'current_user#index'
       resources :products
       resources :orders
       resources :customers
